72ac41582b29d6119d43672f6bc5c054c396f83f
Mark <126978600+markosama@users.noreply.github.com>
2023-03-24 16:20:12 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        GroceryList list = new GroceryList();
-        list.add(new GroceryItemOrder("Apples", 0.5));
-        list.add(new GroceryItemOrder("Bananas", 0.4));
-        list.add(new GroceryItemOrder("Cookies", 2.3));
-        list.add(new GroceryItemOrder("Milk", 1.0));
-        System.out.println("Total cost: $" + list.getTotalCost());
+        System.out.println("Hello world!");
     }
+    public static Integer max(ArrayList<Integer> list) {
+        if (list == null || list.size() == 0) {
+            return null;
+        }
+
+        int max = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            int current = list.get(i);
+            if (current > max) {
+                max = current;
+            }
+        }
+        return max;
 }
